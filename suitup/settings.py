4186ba866fa484ba07d9dcf786fe80d5511e617d
HARIS P A <159028747+harispaprint@users.noreply.github.com>
2025-03-30 05:22:28 +0530

    Update settings.py
    debug False

--- suitup/settings.py
+++ suitup/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 # Use environment variables
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
